a25f3c0786529fb364e7a6b26be8ae3263069919
whibaek <jibaek0513@gmail.com>
2023-01-16 11:21:08 +0900

    [feat] : Config 서버 추가

--- user-service/src/main/java/com/example/userservice/controller/Controller.java
+++ user-service/src/main/java/com/example/userservice/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,12 @@ public class Controller {
     @GetMapping("/info")
     public String info() {
         return "User 서비스 동작합니다";
+    }
+
+    @GetMapping("/config")
+    public String configPage(@Value("${message.owner}") String messageOwner,
+                             @Value("${message.content}") String messageContent){
+        return "Configuration File's Message Owner: " + messageOwner + "\n"
+                + "Configuration File's Message Content: " + messageContent;
     }
 }
